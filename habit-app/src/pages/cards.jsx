a857e3afee6a5149fe3de263891020c917e5f1ad
TerekhovAA <terehov_aa@lgnch.spb.ru>
2025-03-10 12:09:52 +0300

    edited cards page

--- habit-app/src/pages/cards.jsx
+++ habit-app/src/pages/cards.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import * as apiService from '../services/casesService'
+import * as apiService from '../services/cardsService'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './administration.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function Cards() {
 //   const navigate = useNavigate();
   const [items, setItems] = useState([]);
-  const [currentItem, setCurrentItem] = useState({ id: null, name: '' });
+  const [currentItem, setCurrentItem] = useState({ id: null, name: '', description: '' });
   const [isEditing, setIsEditing] = useState(false);
   
   const handleInputChange = (e) => {
@@ -18,12 +18,12 @@ function Cards() {
   const addItem = () => {
     if (!currentItem.name) return;
     async function AddCase() {
-        const room = await apiService.addRoomFromApi(currentItem.name);     
-        setItems([...items, room]);
+        const card = await apiService.addCardFromApi({name: currentItem.name, description: currentItem.description});     
+        setItems([...items, card]);
       }
           
     AddCase();
-    setCurrentItem({ id: null, name: '' });
+    setCurrentItem({ id: null, name: '', description: '' });
   };
 
   const editItem = (item) => {
@@ -38,7 +38,7 @@ function Cards() {
     let result = UpdateCase();
     if(result){
         setItems(items.map(item => (item.id === currentItem.id ? currentItem : item)));
-        setCurrentItem({ id: null, name: '' });
+        setCurrentItem({ id: null, name: '', description: '' });
     }
     setIsEditing(false);
   };
@@ -59,47 +59,59 @@ function Cards() {
 //     navigate(`/rooms/${id}`);
 //   };
 
-//   useEffect(() => {
-//     async function getRooms() {
-//         const rooms = await apiService.getAllRoomsFromApi()
-//         setItems(rooms)
-//       }
+  useEffect(() => {
+    async function getCards() {
+        const cards = await apiService.getCardsFromApi()
+        setItems(cards)
+      }
 
-//     getRooms();
-//   }, [])
+      getCards();
+  }, [])
 
   return (
     <div className="container">
       <h2>Cards for administration 🎴🃏</h2>
-      {/* <div className="input-group">
-        <input
-          type="text"
-          name="name"
-          value={currentItem.name}
-          onChange={handleInputChange}
-          placeholder="Enter item name"
-          className="input"
-        />
-        {isEditing ? (
-          <button onClick={updateItem} className="button"><i className="fa fa-check fa-lg" aria-hidden="true"></i></button>
-        ) : (
-          <button onClick={addItem} className="button"><i className="fa fa-plus fa-lg" aria-hidden="true"></i></button>
-        )}
-      </div> */}
 
-      {/* <table className="u-full-width">
-         <thead>
-           <tr>
-             <th>Name</th>
-             <th>Actions</th>
-           </tr>
-         </thead>
-         <tbody>
+        <div className="input-group">      
+          <input
+            type="text"
+            name="name"
+            value={currentItem.name}
+            onChange={handleInputChange}
+            placeholder="Card Name"
+            className="input"
+          />   
+          <input
+            type="text"
+            name="description"
+            value={currentItem.description}
+            onChange={handleInputChange}
+            placeholder="Card Description"
+            className="input"
+          />
+          {isEditing ? (
+            <button onClick={updateItem} className="button"><i className="fa fa-check fa-lg" aria-hidden="true"></i></button>
+          ) : (
+            <button onClick={addItem} className="button"><i className="fa fa-plus fa-lg" aria-hidden="true"></i></button>
+          )} 
+        </div> 
+
+        <table className="u-full-width">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
             {items.map(item => (
                <tr key={item.id}>
-                 <td>
-                   <button className='button button-clear' onClick={() => showDetail(item.id)}><i className="fa fa-users fa-2x"></i></button>    
+                 <td> 
                    {item.name}                
+                 </td>
+                 <td> 
+                   {item.description}                
                  </td>
                  <td>
                  <div className="button-group">
@@ -110,7 +122,7 @@ function Cards() {
                </tr>
             ))}
          </tbody>
-        </table> */}
+        </table> 
     </div>
   )  
 }
